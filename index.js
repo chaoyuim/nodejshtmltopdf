ba55b4a047b16c90cf2ea6fd8caf0564e2f26884
chao_yu <cyu@smart4solutions.nl>
2021-07-05 20:22:16 +0200

    err

--- index.js
+++ index.js
@@ -36,33 +36,11 @@ app.post('/api/courses', function (req, res) {
 });
 app.post('/api/htmlbody', async function (req, res) {
     const htmlBody = req.body.htmlbody;
-    const file_name = req.body.name || 'unknown';
-    console.log(htmlBody);
 
-    // received the html
-    // use html to create pdf 
-    // dowenload pdf 
-    
-    pdf.createAsync(htmlBody, { format: 'A4', filename: 'something.pdf' }).then(() => {
+    pdf.createAsync(htmlBody, options)
+        .then((pdf) => console.log(pdf.filename));
 
-        res.download(`${__dirname}/something.pdf`, `${file_name}.pdf`, function (err) {
-            if (err) {
-                // Handle error, but keep in mind the response may be partially-sent
-                // so check res.headersSent
-                console.log(err);
-            } else {
-                // decrement a download credit, etc.
-            }
-        })
-    })
-
-    // await pdf.create(htmlBody, options).toFile('./temp.pdf', function (err, res) {
-    //     if (err) {
-    //         res.send(err);
-    //         return console.log(err)
-    //     };
-    //     console.log(res);
-    // });
+    res.send([1, 2, 3]);
 
 });
 
